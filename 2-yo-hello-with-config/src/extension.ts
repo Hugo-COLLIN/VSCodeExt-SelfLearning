109b003e627f2bd19b1eb24eaa92c4d694d1bf4a
Hugo COLLIN <hugo.collin1@outlook.fr>
2026-06-05 15:23:48 +0200

    Hello with config - 2:58-7:55 https://youtu.be/cj-Q6_k5gSM

--- 2-yo-hello-with-config/src/extension.ts
+++ 2-yo-hello-with-config/src/extension.ts
@@ -16,7 +16,8 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('2-jokes.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from 2-jokes!');
+		const greetings = vscode.workspace.getConfiguration().get("greetings");
+		vscode.window.showInformationMessage(`${greetings} tout le monde !!!`);
 	});
 
 	context.subscriptions.push(disposable);
