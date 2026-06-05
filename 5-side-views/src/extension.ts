1c05fa3eb07d5b43c01a305c43afa7ece0940f1d
Hugo COLLIN <hugo.collin1@outlook.fr>
2026-06-05 23:53:16 +0200

    DB list in the ViewContainer activityBar - 21:27-23:26 https://youtu.be/cj-Q6_k5gSM

--- 5-side-views/src/extension.ts
+++ 5-side-views/src/extension.ts
@@ -3,9 +3,14 @@ import { DBDataTreeProvider } from './DBData-TreeProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 	// Lier l'id db-list du package.json au DBDataTreeProvider que l'on a créé
-	const treeView = vscode.window.createTreeView('db-list', {
+	const egTreeView = vscode.window.createTreeView('eg-list', {
 		treeDataProvider: new DBDataTreeProvider()
-	})
+	});
+
+	//DB View
+	const dbTreeView = vscode.window.createTreeView('db-list', {
+		treeDataProvider: new DBDataTreeProvider()
+	});
 }
 
 export function deactivate() {}
